2317a0e15140e61c057de0233bb96f062d5d9d01
KevinKatiraj <katirajk@gmail.com>
2022-09-17 16:22:19 +0200

    day2

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -2,3 +2,46 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+
+let weather = {
+    "apiKey":"c719c8e2e00448f4fa191ae255920084",
+    fetchWeather: function (city) {
+        fetch(
+        "https://api.openweathermap.org/data/2.5/weather?q="
+        + city 
+        + "&units=metric&appid=" 
+        + this.apiKey)
+        .then((response) => response.json())
+        .then((data) => this.displayWeather(data));
+    },
+    displayWeather: function (data) {
+        const { name } = data;
+        const { icon , description } = data.weather[0];
+        const { temp , humidity } = data.main;
+        const { speed } = data.wind;
+        document.querySelector(".city").innerText = "Weather in " + name ;
+        document.querySelector(".icon").src = "https://openweathermap.org/img/wn/" + icon + ".png"
+        document.querySelector(".description").innerText = description;
+        document.querySelector(".temp").innerText = temp + "°C";
+        document.querySelector(".humidity").innerText = "Humidity" + humidity + "%";
+        document.querySelector(".wind").innerText = "Wind Spped " + speed + "km/h";
+        document.querySelector(".weather").classList.remove("loading");
+    },
+    search: function () {
+        this.fetchWeather(document.querySelector(".search-bar").value); 
+    }
+};
+
+document
+.querySelector(".search button")
+.addEventListener("click" , function () {
+    weather.search(); 
+});
+
+document.querySelector(".search-bar").addEventListener("keyup" , function (event) {
+    if (event.key == "Enter") {
+        weather.search();
+    }
+} );
+
+weather.fetchWeather("Tirana");
